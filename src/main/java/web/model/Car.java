a24772ecea2879cc05d13c6e14dbee5d9daf5590
ValyaevRasim <jsm.group@mail.ru>
2022-02-11 16:00:07 +0200

    add interface CarService

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,13 @@
+package web.model;
+
+public class Car {
+    private String model;
+    private String series;
+    private String country;
+
+    public Car(String model, String series, String country) {
+        this.model = model;
+        this.series = series;
+        this.country = country;
+    }
+}
